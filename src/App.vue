0fb04602340a449768aa1993e1429fb6aecb9341
Sunil Sandhu <sunil.sandhu87@gmail.com>
2019-09-28 12:25:27 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDo/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ToDo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,127 @@
+<template>
+  <div id="app">
+    <div class="ToDo">
+      <img class="Logo" :src="logo" alt="Vue logo" />
+      <h1 class="ToDo-Header">Vue To Do</h1>
+      <div class="ToDo-Container">
+        <div class="ToDo-Content">
+          <ToDoItem v-for="todo in list" :todo="todo" @delete="onDeleteItem" :key="todo.id" />
+        </div>
+        <input type="text" v-model="todo" v-on:keyup.enter="createNewToDoItem" />
+        <div class="ToDo-Add" @click="createNewToDoItem()">+</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItem from "./ToDoItem.vue";
+import Logo from "../assets/logo.png";
+
+export default {
+  name: "to-do",
+  components: {
+    ToDoItem
+  },
+  data() {
+    return {
+      list: [{ id: 1, text: "clean the house" }, { id: 2, text: "buy milk" }],
+      todo: "",
+      logo: Logo
+    };
+  },
+
+  methods: {
+    createNewToDoItem() {
+      //validate todo
+      if (!this.todo) {
+        alert("Please enter a todo!");
+        return;
+      }
+
+      const newId = Math.max.apply(null, this.list.map(t => t.id)) + 1;
+      this.list.push({ id: newId, text: this.todo });
+      this.todo = "";
+    },
+    onDeleteItem(todo) {
+      this.list = this.list.filter(item => item !== todo);
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica,
+    Arial, sans-serif;
+  background: linear-gradient(#dcaeff, #ff6a3d);
+  height: auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.Logo {
+  width: 50px;
+  position: relative;
+  top: 50px;
+}
+
+.ToDo {
+  text-align: center;
+  border: 1px solid white;
+  width: 80vw;
+  height: auto;
+  box-shadow: 2px 2px 8px 0px rgba(0, 0, 0, 0.3);
+  background: #fdfdfd;
+  padding-bottom: 60px;
+  margin: 40px auto;
+}
+
+.ToDo-Header {
+  color: black;
+  font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, Helvetica,
+    Arial, sans-serif;
+  font-weight: 400;
+  text-transform: uppercase;
+  margin: 70px auto 30px;
+}
+
+.ToDo-Add {
+  color: white;
+  font-size: 2em;
+  width: 0.5em;
+  height: 0.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  cursor: pointer;
+  background: #73ff73;
+  border-radius: 10px;
+  box-shadow: 1px 1px 1px #47a947;
+  margin: 20px auto 0;
+}
+
+.ToDo-Add:hover {
+  box-shadow: none;
+  margin-top: 21px;
+  margin-left: calc(auto + 1px);
+}
+
+.ToDo-Container {
+  width: 80%;
+  margin: 0 auto;
+}
+
+input {
+  width: 60%;
+  padding: 10px;
+  font-size: 1em;
+  margin: 10px auto;
+  box-shadow: 1px 3px 20px 0px rgba(0, 0, 0, 0.3);
+}
+</style>
