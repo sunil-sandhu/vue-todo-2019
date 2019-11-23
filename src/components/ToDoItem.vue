2556b3c0e41941bc775ee16b895dd844a3cf0b80
Sunil Sandhu <sunil.sandhu87@gmail.com>
2019-11-23 20:46:08 +0100

    accidentally deleted styles from ToDoItem

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -19,9 +19,34 @@ export default {
 </script>
 
 <style>
-.ToDoItem {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+  .ToDoItem {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .ToDoItem-Text {
+    width: 90%;
+    background-color: white;
+    border: 1px solid lightgrey;
+    box-shadow: 1px 1px 1px lightgrey;
+    padding: 12px;
+    margin-right: 10px;
+  }
+
+  .ToDoItem-Delete {
+    padding: 4px 12px;
+    cursor: pointer;
+    background: #ff7373;
+    border-radius: 10px;
+    box-shadow: 1px 1px 1px #c70202;
+    color: white;
+    font-size: 28px;
+  }
+
+  .ToDoItem-Delete:hover {
+    box-shadow: none;
+    margin-top: 1px;
+    margin-left: 1px;
+  }
 </style>
